35b8dfffeb69e2eea84c55e7f1833fffeccfa341
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2021-03-15 16:19:09 -0300

    create styles globals

---
+++ src/styles/Global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    :root {
+        --background: #F0F2F5;
+        --red: #E52E4D;
+        --blue: #5429CC;
+        
+        --blue-light: #6933ff;
+
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --shape: #ffffff;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media(max-width: 1080px){
+            font-size: 93.75%;
+        }
+
+        @media(max-width: 720px){
+            font-size: 87.5px;
+        }
+    }
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased; 
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disable] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
